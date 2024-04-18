b71c608bf2552a2f98946e18acf8982dca55f757
muhammad1308 <muhammad1308>
2024-04-18 16:51:13 +0500

    ok

--- main2.py
+++ main2.py
@@ -1,10 +1,15 @@
 import requests
 
 url = "https://randomuser.me/api/"
-qwer = {'results':3,'gender':'male','nat':'us'}
+qwer = {
+    'results':3,
+    'gender':'male',
+    'nat':''
+    }
 r = requests.get(url,qwer)
 j = r.json()['results']
 
-#print(r.url)
 print(r.status_code)
 print(r.url)
+for i in j:
+    print(i['name'],i['nat'])
